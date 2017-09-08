273dc8f359614eb0775193ed9874a30a458ba27f
Martin Peck <mpeck@hotmail.co.uk>
2017-09-08 16:49:22 +0100

    minor whitespace updates to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app" class="container is-fluid">
+
     <section class="hero is-primary is-bold">
+
       <div class="hero-body">
         <div class="container">
-
           <img class="logo" src="./assets/pokeball.png">
-
           <h1 class="title">
             {{title}}
           </h1>
@@ -17,11 +17,8 @@
       </div>
     </section>
 
-
-
-
-
     <router-view></router-view>
+
   </div>
 </template>
 
